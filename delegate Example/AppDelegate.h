a1e75e6ec1af65bb26f282d9943b5e3d0583572a
kvana <kvanadev1@gmail.com>
2015-08-28 16:40:26 +0530

    Initial Commit

---
+++ delegate Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  delegate Example
+//
+//  Created by Kvana Inc 2 on 28/08/15.
+//  Copyright (c) 2015 Kvana Inc 2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ delegate Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  delegate Example
+//
+//  Created by Kvana Inc 2 on 28/08/15.
+//  Copyright (c) 2015 Kvana Inc 2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
